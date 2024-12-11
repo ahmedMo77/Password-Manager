b542f9cb249374597dc552e21d64916b37d572e6
Ahmed Mohammed <113601141+ahmedMo77@users.noreply.github.com>
2024-12-11 16:24:41 +0200

    Add files via upload

---
+++ EncryptionUtility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassManger.Csharp
+{
+    internal class EncryptionUtility
+    {
+        private static readonly string OriginalChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        private static readonly string alternateChars ="J5rYltxGQM7PCD8KHvbo0shzN9aFIi1LOXjcRdEkWqg6yVBSp4m2u3nZTeAUfw";
+        internal static string Encrypt(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in password)
+            {
+                var charIndex = OriginalChars.IndexOf(item);
+                sb.Append(alternateChars[charIndex]);
+            }
+            return sb.ToString();
+        }
+        internal static string Decrypt(string password)
+        {
+            StringBuilder SB = new StringBuilder();
+            foreach (var item in password)
+            {
+                var charIndex = alternateChars.IndexOf(item);
+                SB.Append(OriginalChars[charIndex]);
+            }
+            return SB.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System.Linq.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PassManger.Csharp
+{
+    internal class Program
+    {
+        private static readonly Dictionary<string, string> _PasswordEntries = new();
+        static void Main(string[] args)
+        {
+            ReadPassword();
+            while (true)
+            {
+                Console.WriteLine("Please select opton: ");
+                Console.WriteLine("1. List All Passwords");
+                Console.WriteLine("2. Add or Change Password");
+                Console.WriteLine("3. Get Password");
+                Console.WriteLine("4. Delete Password");
+                var choose = int.Parse(Console.ReadLine());
+                switch(choose)
+                {
+                    case 1:
+                        ListAllPasswords();
+                        break;
+                    case 2:
+                        Add_or_ChangePassword();
+                        break;
+                    case 3:
+                        GetPassword();
+                        break;
+                    case 4:
+                        DeletePassword();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+                Console.WriteLine("=============================================================");
+            }
+        }
+
+        private static void ListAllPasswords()
+        {
+            foreach (var item in _PasswordEntries)
+            {
+                Console.WriteLine($"{item.Key}={item.Value}");
+            }
+        }
+
+        private static void Add_or_ChangePassword()
+        {
+            Console.Write("Enter the site name: ");
+            var appName = Console.ReadLine();
+            Console.Write("Enter the password: ");
+            var password = Console.ReadLine();
+
+            if(_PasswordEntries.ContainsKey(appName))
+            {
+                _PasswordEntries[appName] = password;
+            }
+            else
+            {
+                _PasswordEntries.Add(appName, password);
+            }
+            SavePassword();
+        }
+
+        private static void GetPassword()
+        {
+            Console.Write("Enter the site name: ");
+            var appName = Console.ReadLine();
+
+            if (_PasswordEntries.ContainsKey(appName))
+            {
+                Console.WriteLine($"Yuor password is : '{_PasswordEntries[appName]}'");
+            }
+            else
+            {
+                Console.WriteLine("Password not found");
+            }
+        }
+
+        private static void DeletePassword()
+        {
+            Console.Write("Enter the Application name : ");
+            var appName = Console.ReadLine();
+
+            if (_PasswordEntries.ContainsKey(appName))
+            {
+                _PasswordEntries.Remove(appName);
+                SavePassword();
+            }
+            else
+            {
+                Console.WriteLine("Password not found");
+            }
+        }
+        private static void ReadPassword()
+        {
+            if(File.Exists("passwords.txt"))
+            {
+                var PasswordLines = File.ReadAllText("passwords.txt");
+                foreach (var line in PasswordLines.Split(Environment.NewLine))
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        var equalIndex = line.IndexOf('=');
+                        var appName = line.Substring(0, equalIndex);
+                        var password = line.Substring(equalIndex + 1);
+                        _PasswordEntries.Add(appName, EncryptionUtility.Decrypt(password));
+                    }
+                }
+            }
+        }
+
+        private static void SavePassword()
+        {
+            StringBuilder PasswordLines = new StringBuilder();
+            foreach (var item in _PasswordEntries)
+            {
+                PasswordLines.AppendLine($"{item.Key}={EncryptionUtility.Encrypt(item.Value)}");
+            }
+            File.WriteAllText("passwords.txt", PasswordLines.ToString());
+        }
+    }
+}
